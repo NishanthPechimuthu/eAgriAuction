fb506091e502abfc054dced29881f137219b11a9
NishanthPechimuthu <nishanthpechimuthu@gmail.com>
2024-12-07 15:50:20 +0530

    Public Folder Completed and Admin Folder Started

--- public/ma.php
+++ public/ma.php
@@ -136,14 +136,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
-    <script>
-        window.addEventListener('DOMContentLoaded', event => {
-            const datatablesSimple = document.getElementById('auctionsTable');
-            if (datatablesSimple) {
-                new simpleDatatables.DataTable(datatablesSimple);
-            }
-        });
-    </script>
+<script>
+    window.addEventListener('DOMContentLoaded', event => {
+        const datatablesSimple = document.getElementById('auctionsTable');
+        if (datatablesSimple) {
+            new simpleDatatables.DataTable(datatablesSimple, {
+                columnDefs: [
+                    { orderable: false, targets: [8, 9, 10] } // Disable ordering for "View", "Edit", "Delete" columns
+                ]
+            });
+        }
+    });
+</script>
 </body>
 </html>
 
